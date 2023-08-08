892742ae552523eef17e63a32733522be372eefc
LetyMores <lety_mores@outlook.com>
2023-08-08 17:14:37 -0300

    Revert "Revert "Update Alumnos.java""
    This reverts commit 7a6481b47196fd1a30a2b1bab30742f922ac15a3.

--- Prueba GrupalGit/src/prueba/grupalgit/Alumnos.java
+++ Prueba GrupalGit/src/prueba/grupalgit/Alumnos.java
@@ -11,5 +11,13 @@ package prueba.grupalgit;
 public class Alumnos {
     
     private String nombre;
+    private String apellido;
+
+    public Alumnos(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
     
 }
