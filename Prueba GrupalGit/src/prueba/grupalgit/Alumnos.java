c98794a85b33c3a399aa704437272a87599c2f4c
amaru44 <ofereus@gmail.com>
2023-08-09 16:49:36 -0300

    agregado comentarios que describen la clase y sus métodos, lo que hace que el código sea más claro para otros desarrolladores que puedan leerlo en el futuro. También he incluido métodos de acceso (get y set) para los campos nombre y apellido, lo que permite obtener y establecer estos valores de manera controlada. Además, he agregado un método toString() para proporcionar una representación en cadena del objeto Alumnos.

--- Prueba GrupalGit/src/prueba/grupalgit/Alumnos.java
+++ Prueba GrupalGit/src/prueba/grupalgit/Alumnos.java
@@ -5,7 +5,9 @@
 package prueba.grupalgit;
 
 /**
- *
+ * Clase que representa a un estudiante.
+ * Esta clase almacena el nombre y el apellido de un estudiante.
+ * 
  * @author LENOVO
  */
 public class Alumnos {
@@ -13,11 +15,60 @@ public class Alumnos {
     private String nombre;
     private String apellido;
 
+    /**
+     * Constructor de la clase Alumnos.
+     * 
+     * @param nombre El nombre del estudiante.
+     * @param apellido El apellido del estudiante.
+     */
     public Alumnos(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
     }
-    
-    
-    
+
+    /**
+     * Obtiene el nombre del estudiante.
+     * 
+     * @return El nombre del estudiante.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establece el nombre del estudiante.
+     * 
+     * @param nombre El nuevo nombre del estudiante.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Obtiene el apellido del estudiante.
+     * 
+     * @return El apellido del estudiante.
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * Establece el apellido del estudiante.
+     * 
+     * @param apellido El nuevo apellido del estudiante.
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * Devuelve una representación en cadena del objeto Alumnos.
+     * 
+     * @return Una cadena que muestra el nombre y el apellido del estudiante.
+     */
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Apellido: " + apellido;
+    }
 }
